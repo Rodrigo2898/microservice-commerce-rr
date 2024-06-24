091404e735a34901d951c5b61ce09f0aea78f9a9
Rodrigo2898 <rodrigofeitosarodrigues@gmail.com>
2024-06-23 21:56:21 -0300

    Teste de integração para ProductService

--- product-service/src/test/java/com/msrr/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/msrr/product_service/ProductServiceApplicationTests.java
@@ -1,19 +1,44 @@
 package com.msrr.product_service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.msrr.product_service.dto.ProductRequest;
+import com.msrr.product_service.model.Product;
+import com.msrr.product_service.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
 	@Container
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo");
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+	@Autowired
+	private ProductRepository productRepository;
 
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
@@ -21,7 +46,21 @@ class ProductServiceApplicationTests {
 	}
 
 	@Test
-	void contextLoads() {
+	public void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString))
+				.andExpect(status().isCreated());
+		Assertions.assertEquals(1, productRepository.findAll().size());
 	}
-
+	
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("Samsung Galaxy")
+				.description("Novo Samsung Galaxy")
+				.price(BigDecimal.valueOf(5000))
+				.build();
+	}
 }
