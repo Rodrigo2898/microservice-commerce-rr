5512d740168be55d3433b9a1fd550db7114265a0
Rodrigo2898 <rodrigofeitosarodrigues@gmail.com>
2024-07-01 23:24:06 -0300

    Configurações gerais no pom.xml e inicio da implementação do service discovery

--- inventory-service/src/main/java/com/msrr/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/msrr/inventory_service/InventoryServiceApplication.java
@@ -23,7 +23,7 @@ public class InventoryServiceApplication {
 
 			Inventory inventory1 = new Inventory();
 			inventory1.setSkuCode("Samsung_novo");
-			inventory1.setQuantity(100);
+			inventory1.setQuantity(0);
 
 			inventoryRepository.save(inventory);
 			inventoryRepository.save(inventory1);
