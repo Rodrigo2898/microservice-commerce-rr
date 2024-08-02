2ff54dafc4e27ddfc98f9413e57fc32b64bf973c
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2024-08-01 21:46:02 -0300

    Inicio implementação API-GATEWAY

--- api-gateway/src/main/java/com/msrr/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/msrr/api_gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.msrr.product_service;
+package com.msrr.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
-	}
-
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
--- order-service/src/main/java/com/msrr/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/msrr/order_service/OrderServiceApplication.java
@@ -2,10 +2,10 @@ package com.msrr.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
